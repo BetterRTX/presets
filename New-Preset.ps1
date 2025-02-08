0849d5afacf2305be5c137f24b017edbd7c5749b
Jason Gardner <im@jasongardner.co>
2025-02-08 11:46:20 -0600

    upd: Placeholder frontmatter content

--- New-Preset.ps1
+++ New-Preset.ps1
@@ -13,12 +13,19 @@ function New-Preset {
     # Create README.md
     $readmeContent = @"
 ---
-name: "My Preset"
-installerTitle: "My Preset"
+name: "Website Friendly Title"
+installerTitle: "The Text Displayed in the Installer"
 version: 1.0.0
 brtxVersion: 1.3
-lastUpdated: $(Get-Date -Format "yyyy-MM-dd")
-author: Your Name
+lastUpdated: 2025-02-14
+author: Not Mojang
+hash: [Optional settings hash from Creator output]
+rtxStub: [RTXStub.material.bin URL]
+rtxChecksum: [SHA-256 file chucksum]
+toneMappingStub: [RTXPostFXTonemapping.material.bin URL]
+toneMappingChecksum: [SHA-256 file chucksum]
+bloomStub: [RTXPostFX.Bloom.material.bin URL]
+bloomChecksum: [SHA-256 file chucksum]
 ---
 
 # My Preset
